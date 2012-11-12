477b8ef194399c5dfc8a10d46bfb0617ab19ac17
Chris Lloyd <christopher.lloyd@gmail.com>
2012-11-12 15:38:50 -0800

    Extracts `generate_unique` for use in migrations.

--- lib/uniquify.rb
+++ lib/uniquify.rb
@@ -9,20 +9,33 @@ module Uniquify
     end while self.class.exists?(name => self[name])
   end
 
+  def generate_unique(name, options = {}, &block)
+    options = self.class.default_uniquify_options.merge(options)
+
+    if block
+      ensure_unique(name, &block)
+    else
+      ensure_unique(name) do
+        Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
+      end
+    end
+  end
+
+
   module ClassMethods
 
+    def default_uniquify_options
+      {
+        :length => 8,
+        :chars => ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+      }
+    end
+
     def uniquify(*args, &block)
-      options = { :length => 8, :chars => ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a }
-      options.merge!(args.pop) if args.last.kind_of? Hash
+      options = args.pop if args.last.kind_of? Hash
       args.each do |name|
         before_validation :on => :create do |record|
-          if block
-            record.ensure_unique(name, &block)
-          else
-            record.ensure_unique(name) do
-              Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
-            end
-          end
+          record.generate_unique(name, options, &block)
         end
       end
     end
